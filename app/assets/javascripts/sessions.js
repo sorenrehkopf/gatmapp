c9078d40aaca042282b159af95eab6a0e170a539
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-07-30 22:08:15 -0700

    search scroll fixed for real this time

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -26,7 +26,7 @@ $(function(){
 					$.get(url,data,function(giphs1){
 						console.log(giphs1);
 						for(i=0;i<giphs1.data.length;i++){
-							$('#searchResults').append('<div class="imageDiv" imgurl="'+giphs1.data[i].images.original.url+'" style="background-image: url('+giphs.data[i].images.original.url+');"><img class="show" src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672382-expand-128.png"><img class="save" src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/add-128.png"><img class="post" src="https://d30y9cdsu7xlg0.cloudfront.net/png/1490-200.png"></div>' );
+							$('#searchResults').append('<div class="imageDiv" imgurl="'+giphs1.data[i].images.original.url+'" style="background-image: url('+giphs1.data[i].images.original.url+');"><img class="show" src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672382-expand-128.png"><img class="save" src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/add-128.png"><img class="post" src="https://d30y9cdsu7xlg0.cloudfront.net/png/1490-200.png"></div>' );
 						};
 						more = true
 					});
