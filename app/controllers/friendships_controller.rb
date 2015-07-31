527aac2ef776fe2d8b038fe2fa80f76da7a2c664
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-07-31 11:37:18 -0700

    duplicative following fixed.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,13 @@
 class FriendshipsController < ApplicationController
 
 	def create
-
-		@friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-		if @friendship.save
-			redirect_to sessions_path
+		if !current_user.friendships.find_by_friend_id(params[:friend_id])
+			@friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+			if @friendship.save
+				redirect_to sessions_path
+			else
+				redirect_to sessions_path
+			end
 		else
 			redirect_to sessions_path
 		end
